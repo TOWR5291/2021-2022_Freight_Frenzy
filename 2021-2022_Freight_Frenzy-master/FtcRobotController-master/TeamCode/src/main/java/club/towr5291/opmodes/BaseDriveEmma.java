6377b2117871eadfc8c8d126dc76d586d7da1f64
Alx <ARustoni19@gmail.com>
2021-10-09 13:55:55 -0400

    AKR - TEST1

--- 2021-2022_Freight_Frenzy-master/FtcRobotController-master/TeamCode/src/main/java/club/towr5291/opmodes/BaseDriveEmma.java
+++ 2021-2022_Freight_Frenzy-master/FtcRobotController-master/TeamCode/src/main/java/club/towr5291/opmodes/BaseDriveEmma.java
@@ -28,6 +28,7 @@ import club.towr5291.robotconfig.HardwareDriveMotors;
 
 /*
     made by Emma Beggs 11-15-20  ^^^^
+    Note added by AKR
 */
 @TeleOp(name = "BaseDriveEmma", group = "5291")
 public class BaseDriveEmma extends OpModeMasterLinear {
